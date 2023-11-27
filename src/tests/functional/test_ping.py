7fadd50c3cff1fd8dd6b3e9aac83530d579173fd
waffe_x20 <argueta_eduardo@yahoo.de>
2023-11-26 23:31:15 -0600

    Add test for user

--- src/tests/functional/test_ping.py
+++ src/tests/functional/test_ping.py
@@ -6,10 +6,22 @@ def test_ping(test_app):
     client = test_app.test_client()
 
     # When
-    resp = client.get('/ping')
+    resp = client.get("/ping")
     data = json.loads(resp.data.decode())
 
     # Then
     assert resp.status_code == 200
-    assert 'Urantia' in data['message']
-    assert 'success' in data['status']
+    assert "Urantia" in data["message"]
+    assert "success" in data["status"]
+
+
+def test_add_user_invalid_json(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        "/users",
+        data=json.dumps({}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Input payload validation failed" in data["message"]
