cf568becfb71a8b5e517ccaa5cba1f5376d0c2e2
waffe_x20 <argueta_eduardo@yahoo.de>
2023-11-27 02:35:32 -0600

    Refactor test add user to test_users

--- src/tests/functional/test_ping.py
+++ src/tests/functional/test_ping.py
@@ -13,15 +13,3 @@ def test_ping(test_app):
     assert resp.status_code == 200
     assert "Urantia" in data["message"]
     assert "success" in data["status"]
-
-
-def test_add_user_invalid_json(test_app, test_database):
-    client = test_app.test_client()
-    resp = client.post(
-        "/users",
-        data=json.dumps({}),
-        content_type="application/json",
-    )
-    data = json.loads(resp.data.decode())
-    assert resp.status_code == 400
-    assert "Input payload validation failed" in data["message"]
