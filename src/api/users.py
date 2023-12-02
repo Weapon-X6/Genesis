aa5a7a3b51438c5ba6fe335c569d312ff9c13c66
waffe_x20 <argueta_eduardo@yahoo.de>
2023-12-01 23:06:45 -0600

    Add handling of user exceptions and erros by post

--- src/api/users.py
+++ src/api/users.py
@@ -1,29 +1,43 @@
 from flask import Blueprint, request
-from flask_restx import Resource, Api
+from flask_restx import Resource, Api, fields
 
 from src import db
 from src.api.models import User
 
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint("users", __name__)
 api = Api(users_blueprint)
 
 
-class UsersList(Resource):
+user = api.model(
+    "User",
+    {
+        "id": fields.Integer(readOnly=True),
+        "username": fields.String(required=True),
+        "email": fields.String(required=True),
+        "created_date": fields.DateTime,
+    },
+)
 
+
+class UsersList(Resource):
+    @api.expect(user, validate=True)
     def post(self):
         post_data = request.get_json()
-        username = post_data.get('username')
-        email = post_data.get('email')
+        username = post_data.get("username")
+        email = post_data.get("email")
+        response_object = {}
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            response_object["message"] = "Sorry. That email already exists."
+            return response_object, 400
 
         db.session.add(User(username=username, email=email))
         db.session.commit()
 
-        response_object = {
-            'message': f'{email} was added!'
-        }
-
+        response_object["message"] = f"{email} was added!"
         return response_object, 201
 
 
-api.add_resource(UsersList, '/users')
+api.add_resource(UsersList, "/users")
--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -27,3 +27,40 @@ def test_add_user_invalid_json(test_app, test_database):
 
     assert resp.status_code == 400
     assert "Input payload validation failed" in data["message"]
+
+
+def test_add_user_invalid_json_keys(test_app, test_database):
+    client = test_app.test_client()
+
+    resp = client.post(
+        "/users",
+        data=json.dumps({"email": "itfeels@like.de"}),
+        content_type="application/json",
+    )
+
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Input payload validation failed" in data["message"]
+
+
+def test_add_user_duplicate_email(test_app, test_database):
+    client = test_app.test_client()
+
+    client.post(
+        "/users",
+        data=json.dumps(
+            {"username": "mf", "email": 'email": "whatmakesyouthink@imcoming.de'}
+        ),
+        content_type="application/json",
+    )
+    res = client.post(
+        "/users",
+        data=json.dumps(
+            {"username": "mf", "email": 'email": "whatmakesyouthink@imcoming.de'}
+        ),
+        content_type="application/json",
+    )
+
+    data = json.loads(res.data.decode())
+    assert res.status_code == 400
+    assert "Sorry. That email already exists." in data["message"]
